7f06407eb28d32cf0b77b2465c9e752ef9dd1dba
Idrisov Daniyal <daniyalou@gmail.com>
2023-04-24 13:43:01 +0300

    Vavilon v_2.1

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import style from './Movie.module.css';
-import Booking from '../Booking/Booking';
-import { Link } from 'react-router-dom';
-import { UrlMovie } from '../../urls';
+import React from "react";
+import style from "./Movie.module.css";
+import Booking from "../Booking/Booking";
+import { Link } from "react-router-dom";
+import { UrlMovie } from "../../urls";
 
 const Movie = ({ movieId, price, time, sessionId }) => {
   const maxLength = 100;
@@ -12,32 +12,35 @@ const Movie = ({ movieId, price, time, sessionId }) => {
   React.useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(UrlMovie + '/' + movieId);
+        const response = await fetch(UrlMovie + "/" + movieId);
         const data = await response.json();
         setMovieList(data);
       } catch (error) {
-        console.error('There was a problem fetching the data:', error);
+        console.error("There was a problem fetching the data:", error);
       }
     };
-
+    console.log(movieList);
     fetchData();
   }, []);
 
   return (
     <div className={style.movie}>
       <div className={style.movieG}>
-        <img
-          src={movieList.poster_url}
-          alt={movieList.title}
-          className={style.moviePoster}
-        />
+        <Link to={`movies/${movieId}`}>
+          <img
+            src={movieList.poster_url}
+            alt={movieList.title}
+            className={style.moviePoster}
+          />
+        </Link>
+
         <div className={style.movieInfo}>
           <h2 className={style.movieTitle}>{movieList.title}</h2>
-          <p className={style.movieYear}>Год: </p>
+          <p className={style.movieYear}>Год: {movieList.year}</p>
 
           <div className={style.moviePT}>
             <p className={style.moviePrice}>
-              Цена: {price.replace('.00', '')} ₽
+              Цена: {price.replace(".00", "")} ₽
             </p>
             <p className={style.movieTime}>Время: {time}</p>
           </div>
@@ -46,10 +49,10 @@ const Movie = ({ movieId, price, time, sessionId }) => {
             <br />
             {movieList.description && movieList.description.length > maxLength
               ? `${movieList.description.substring(0, maxLength)}...`
-              : movieList.description}{' '}
+              : movieList.description}{" "}
             <Link to={`movies/${movieId}`}>Далее</Link>
           </p>
-          <p className={style.movieGenre}>Жанр: </p>
+          <p className={style.movieGenre}>Жанр:{movieList.genre} </p>
 
           <div className={style.movieRaR}>
             <p className={style.movieRating}>Рейтинг: {movieList.rating}</p>
