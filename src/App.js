2874bae5b098a7b9e5e8b1948a581266859885f9
root <root@1403459-cy13881.tw1.ru>
2023-04-25 18:01:47 +0300

    Vavilon v_2.3

--- src/App.js
+++ src/App.js
@@ -6,34 +6,11 @@ import MovieItem from "./components/MovieItem/MovieItem";
 import AdminCustomer from "./components/Admin/AdminCustomer/AdminCustomer";
 import AdminMovieList from "./components/Admin/AdminMovieList/AdminMovieList";
 import AdminMoviePoster from "./components/Admin/AdminMoviePoster/AdminMoviePoster";
-import io from 'socket.io-client';
 
-const ENDPOINT = 'http://kinovavilon.ru:5000';
 
 
 function App() {
-  React.useEffect(() => {
-    const socket = io(ENDPOINT, {
-      transports: ['websocket']
-    });
-
-    socket.on('connect', () => {
-      console.log('Connected to server');
-      socket.emit('eventFromClient', 'Hello from client');
-    });
-
-    socket.on('eventFromServer', (data) => {
-      console.log(data);
-    });
-
-    socket.on('disconnect', () => {
-      console.log('Disconnected from server');
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+   
   return (
     <BrowserRouter>
       <Routes>
