8a7272f5772ce678d6b373e1125c2a1c9687e3b7
Idrisov Daniyal <daniyalou@gmail.com>
2023-05-03 14:47:31 +0300

    Vavilon v_3.5

--- src/components/Admin/AdminCustomer/CustomerItem/CustomerItem.jsx
+++ src/components/Admin/AdminCustomer/CustomerItem/CustomerItem.jsx
@@ -1,34 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import style from './CustomerItem.module.css';
-import { UrlSession, UrlMovie } from '../../../../urls';
+import React, { useEffect, useState } from "react";
+import style from "./CustomerItem.module.css";
+import { UrlSession, UrlMovie } from "../../../../urls";
 
 const CustomerItem = ({ sessionId, countPerson }) => {
+  console.log(countPerson);
   const [sessionData, setSessionData] = useState([]);
   const [movieData, setMovieData] = useState([]);
 
   const fetchSessionData = async () => {
     try {
-      const response = await fetch(UrlSession + '/' + sessionId);
+      const response = await fetch(UrlSession + "/" + sessionId);
       const data = await response.json();
       setSessionData(data);
       fetchMovieData(data.movieId);
     } catch (error) {
-      console.error('Failed to fetch session data', error);
+      console.error("Failed to fetch session data", error);
     }
   };
 
   const fetchMovieData = async (movieId) => {
     try {
-      const response = await fetch(UrlMovie + '/' + movieId);
+      const response = await fetch(UrlMovie + "/" + movieId);
       const data = await response.json();
       setMovieData(data);
     } catch (error) {
-      console.error('Failed to fetch movie data', error);
+      console.error("Failed to fetch movie data", error);
     }
   };
 
   useEffect(() => {
     fetchSessionData();
+  }, []);
+
+  useEffect(() => {
+    const fetchSessionDataPut = async () => {
+      try {
+        const response = await fetch(UrlSession + "/" + sessionId, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            remaining_seats: countPerson,
+          }),
+        });
+        const data = await response.json();
+        console.log("Данные успешно обновлены:", data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchSessionDataPut();
   }, []);
 
   return (
